d6d36f807fda10db034a93594b4c0d9cd038fe3a
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-10-30 15:05:57 +0300

    update 1.1.1.2

--- src/For_me.kt
+++ src/For_me.kt
@@ -3,6 +3,7 @@ fun main(){
     //println((1..3).toList())
     val workers= mutableListOf<Worker>()
     workers.add(Programmer("Jamoliddin",20,"Java and Kotlin"))
+    workers.add(Programmer("Firdavs",19,"Java, Kotlin and Python"))
     workers.add(Programmer("Sunat",20,"Java and PHP"))
     workers.add(Programmer("Behzod",21,"C++ and PHP"))
     workers.add(Teacher("Husniddin",20,"Informatic"))
