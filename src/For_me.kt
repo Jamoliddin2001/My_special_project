0e4636f496dcf9735a71cff244cc9a68377983ca
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-11-05 22:07:13 +0300

    update 1.1.1.2.9

--- src/For_me.kt
+++ src/For_me.kt
@@ -8,7 +8,7 @@ fun main(){
     workers.add(Programmer("Dovar",20,"JavaScript and Go"))
     workers.add(Programmer("Behzod",21,"C++ and PHP"))
     workers.add(Programmer("Husniddin",20,"C++, Java, Python, Tajik"))
-    //workers.add(Teacher("Hikmatullo",26,"Mathematic"))
+    workers.add(Teacher("Hikmatullo",26,"Mathematic"))
 
     println((Math.random()*100).toString())
 
