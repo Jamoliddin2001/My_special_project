b4ffab3f7e62465378b22acaf8ead360469d4c81
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-11-10 18:24:48 +0300

    update 1.1.1.2.16

---
+++ src/Ali.java
@@ -0,0 +1,23 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Ali {
+
+    public static void main(String[] args) {
+
+        int N;
+
+        Scanner scan=new Scanner(System.in);
+        N=scan.nextInt();
+
+        int[] mass = new int[N];
+        for(int i = 0 ; i<N;i++){
+            mass[i] = scan.nextInt();
+        }
+        for(int i = N-1 ; i>=0;i--){
+            System.out.print(mass[i]+"     ");
+        }
+
+    }
+}
+
