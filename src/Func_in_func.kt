cffb8ee1e900aef3906c01eee9b96cd8b8c5711b
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-11-11 15:29:23 +0300

    update 1.1.1.2.18

---
+++ src/Func_in_func.kt
@@ -0,0 +1,34 @@
+/*fun main() {
+
+    displayMessage(::morning)
+    displayMessage(::evening)
+}
+fun displayMessage(mes: () -> Unit){
+    mes()
+}
+fun morning(){
+    println("Good Morning")
+}
+fun evening(){
+    println("Good Evening")
+}*/
+fun main() {
+
+    action(5, 3, ::sum)         // 8
+    action(5, 3, ::multiply)    // 15
+    action(5, 3, ::subtract)    // 2
+}
+
+fun action (n1: Int, n2: Int, op: (Int, Int)-> Int){
+    val result = op(n1, n2)
+    println(result)
+}
+fun sum(a: Int, b: Int): Int{
+    return a + b
+}
+fun subtract(a: Int, b: Int): Int{
+    return a - b
+}
+fun multiply(a: Int, b: Int): Int{
+    return a * b
+}
